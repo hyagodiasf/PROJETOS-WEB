3ec85c0d11887a3912353b65297e86c5230bc421
Hyago Diaz <hyagodiasf@gmail.com>
2022-07-17 14:15:13 -0300

    jogo de acertar palavras - html-css-js

---
+++ ENCONTRE-PALAVRAS/assets/js/main.js
@@ -0,0 +1,63 @@
+const listaPalavras = [
+    {
+        palavra: "Disturbed",
+        dica: "Banda de Rock Internacional"
+    },
+    {
+        palavra: "Nike",
+        dica: "Marca famosa de futebol"
+    },
+    {
+        palavra: "Star Wars",
+        dica: "Guerras e lasers de luz"
+    },
+    {
+        palavra: "Inferno",
+        dica: "Enigma de uma obra famosa"
+    },
+    {
+        palavra: "Paulo Coelho",
+        dica: "Famoso por escrever o Alquimista"
+    },
+    {
+        palavra: "Ypióca",
+        dica: "Tem prata, ouro e empalhada"
+    },
+    {
+        palavra: "Jogo da Imitação",
+        dica: "Decifra cóigos militares"
+    },
+    {
+        palavra: "Misfits",
+        dica: "Horror Punk"
+    },
+    {
+        palavra: "Crato",
+        dica: "Município famosa por exposição agropecuária"
+    },
+    {
+        palavra: "Brasil",
+        dica: "País do futebol"
+    },
+];
+
+const resul = document.querySelector(".inputs"),
+reset = document.querySelector(".btn-reset"),
+dica = document.querySelector(".dica span"),
+digita = document.querySelector(".digita");
+
+function radomPalavra(){
+    const ranP = listaPalavras[Math.floor(Math.random() * listaPalavras.length)];
+    const palavra = ranP.palavra;
+    dica.innerHTML = ranP.dica;
+    let html = "";
+
+    for (let i = 0; i < palavra.length; i++) {
+        html += `<input type="text" disabled>`;
+    }
+    resul.innerHTML = html;
+}
+radomPalavra();
+reset.addEventListener("click", radomPalavra);
+digita.document.addEventListener("input", initGame);
+document.addEventListener("keydown", () => digita.focus());
