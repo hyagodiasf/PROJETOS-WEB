6ed42c5003629ffaa9452588f0d7cc397229e608
Hyago Diaz <hyagodiasf@gmail.com>
2022-07-19 11:43:27 -0300

    Criando um relógio com HTML-CSS3-JS

---
+++ RELÓGIO/assets/js/main.js
@@ -0,0 +1,29 @@
+//aqui pega a ag h1 onde vai ficar a numeração do relógio
+const timer = document.querySelector("h1");
+//for para a option de horas
+
+setInterval(() => {
+    //pegar hora, minutos e segundos
+    let times = new Date(),
+    hora = times.getHours(),
+    minutos = times.getMinutes(),
+    segundos = times.getSeconds(),
+    ampm = "AM";
+
+    if(hora >= 12){hora = hora - 12; ampm = "PM";}
+
+    hora = hora == 0 ? hora = 12 : hora;
+    hora = hora < 10 ? "0" + hora : hora;
+    minutos = minutos < 10 ? "0" + minutos : minutos;
+    segundos = segundos < 10 ? "0" + segundos : segundos;
+
+    // if(hora == 0){ hora = 12; }else{ hora = 12}
+
+    // if(hora < 10){ hora = "0" + hora; }else{hora = }
+
+    // if(minutos < 10){ minutos = "0" + minutos; }
+
+    // if(segundos < 10){ segundos = "0" + minutos;}
+
+    timer.innerHTML = `${hora}:${minutos}:${segundos} ${ampm}`;
+}, 1000);
