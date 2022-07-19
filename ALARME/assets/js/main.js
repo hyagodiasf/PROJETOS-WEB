9f86b0ea52fd0087c6df9830c56b42c3c8e970fc
Hyago Diaz <hyagodiasf@gmail.com>
2022-07-19 11:33:38 -0300

    Modificação no script.js para definir o setInterval para o alarme

--- ALARME/assets/js/main.js
+++ ALARME/assets/js/main.js
@@ -1,6 +1,6 @@
 //pega a tag select do index.html
 const selete = document.querySelectorAll("select");
-const time = document.querySelectorAll("h1");
+const timer = document.querySelector("h1");
 //for para a option de horas
 for (let i = 12; i > 0; i--) {
     if(i < 10 ){
@@ -52,10 +52,22 @@ setInterval(() => {
     hora = times.getHours(),
     minutos = times.getMinutes(),
     segundos = times.getSeconds(),
-    am = "AM";
+    ampm = "AM";
 
-    if(hora >= 12){
-        hora = hora - 12;
-        pm = "PM"
-    }
+    if(hora >= 12){hora = hora - 12; ampm = "PM";}
+
+    hora = hora == 0 ? hora = 12 : hora;
+    hora = hora < 10 ? "0" + hora : hora;
+    minutos = minutos < 10 ? "0" + minutos : minutos;
+    segundos = segundos < 10 ? "0" + segundos : segundos;
+
+    // if(hora == 0){ hora = 12; }else{ hora = 12}
+
+    // if(hora < 10){ hora = "0" + hora; }else{hora = }
+
+    // if(minutos < 10){ minutos = "0" + minutos; }
+
+    // if(segundos < 10){ segundos = "0" + minutos;}
+
+    timer.innerHTML = `${hora}:${minutos}:${segundos} ${ampm}`;
 }, 1000);
